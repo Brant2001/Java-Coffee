0b90f79d0faed99d9414d77e6b6d96affa9a53e0
Brant2001 <brant.pippin@gmail.com>
2020-03-06 15:51:23 -0600

    styled main page and added a header

---
+++ scripts/nav/Header.js
@@ -0,0 +1,10 @@
+// const Header = (headerObject) => {
+//     return `
+//         <section>
+//             <h1>Javasips by Madi</h1>
+//         </section>
+//     `
+// }
+
+// const contentTarget = document.querySelector(".header")
+
